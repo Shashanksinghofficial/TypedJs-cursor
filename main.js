8d5b1da43c797dd78e09d2b644836932dad8b252
Ankit Kosta <codetive3@gmail.com>
2024-10-24 12:14:32 +0530

    upload cursor TypedJs file

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typed-chat", {
+    strings: ["Hello! How can I assist you today?", "What would you like to know?", "Feel free to ask any question!"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    /*loop stop here by true,startDelay,backDelay value */
+    loop:false,
+    showCursor:true,
+  cursorChar: "❗", // Change the cursor symbol
+  autoInsertCss:true
+  });
